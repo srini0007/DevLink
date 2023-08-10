1ee87ea40c6dc450225bb5acceeb16f3d6971b39
srini0007 <111696400+srini0007@users.noreply.github.com>
2023-08-10 20:06:10 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 
 const connectDB =async()=>{
     try{
-        // console.log(process.env.MONGO_URI);
-        const conn=await mongoose.connect("mongodb+srv://backupshello:backups%40123@cluster0.6qxecg4.mongodb.net");
+        // console.log();
+        const conn=await mongoose.connect(process.env.MONGO_URI);
         console.log(`mongo connected: ${conn.connection.host}`)
     }
     catch(err){
